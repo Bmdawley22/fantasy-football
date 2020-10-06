0a7c85e674b82068e97cd675dd056ea0447e20c3
Brady Dawley <dawleybradym@johndeere.com>
2020-10-06 14:25:54 -0500

    added order roster function (not completed)

--- controllers/rosters.js
+++ controllers/rosters.js
@@ -1,6 +1,7 @@
 const User = require('../models').Users;
-const Roster = require('../models').Rosters;
 const Player = require('../models').Player;
+
+
 
 const rendRoster = (req,res) => {
     User.findByPk(req.user.id, {
@@ -12,9 +13,11 @@ const rendRoster = (req,res) => {
         ]
     })
     .then(user => {
-            res.render('main/roster.ejs', {
-                user: user
-            });
+        orderedRoster = orderRoster(user);
+        res.render('main/roster.ejs', {
+            user: user,
+            roster: orderedRoster
+        });
     }) 
 }
 
@@ -88,4 +91,78 @@ module.exports = {
     rendOtherTeam,
     dropPlayer,
     addPlayer
+}
+
+function orderRoster(user) {
+    const rosterArray = [];
+    let qbCount = 0;
+    let wrCount = 0;
+    let rbCount = 0;
+    let teCount = 0;
+    let flexCount = 0;
+    let dstCount = 0;
+    let kCount = 0;
+    for (let i = 0; i < user.Players.length; i++) {
+        if(user.Players[i].position === "QB" && qbCount < 1) {
+            rosterArray.push(user.Players[i]);
+            qbCount++;
+        }
+    }
+    for (let i = 0; i < user.Players.length; i++) {
+        if(user.Players[i].position === "RB" && rbCount < 2 ) {
+            rosterArray.push(user.Players[i]);
+            rbCount++;
+        } else if (user.Players[i].position === "RB" && rbCount === 2 ) {
+            rbCount++;
+        }
+    }
+    for (let i = 0; i < user.Players.length; i++) {
+        if(user.Players[i].position === "WR" && wrCount < 2 ) {
+                rosterArray.push(user.Players[i]);
+                wrCount++;
+        }
+        else if (user.Players[i].position === "WR" && wrCount === 2 ) {
+            wrCount++;
+        }
+    }
+    for (let i = 0; i < user.Players.length; i++) {
+        if(user.Players[i].position === "TE" && teCount < 1 ) {
+                rosterArray.push(user.Players[i]);
+                teCount++;
+        }
+        else if (user.Players[i].position === "TE" && teCount === 1 ) {
+            teCount++;
+        }
+    }
+    rbCount2 = 0;
+    wrCount2 = 0;
+    teCount2 = 0;
+    for (let i = 0; i < user.Players.length; i++) {
+        if(flexCount <  1) {
+            if((user.Players[i].position === "RB" && rbCount === 3) ||
+               (user.Players[i].position === "WR" && wrCount === 3) ||
+               (user.Players[i].position === "TE" && teCount === 2)) {
+            
+                if((user.Players[i].position === "RB" && rbCount === 3) ||
+                (user.Players[i].position === "WR" && wrCount === 3) ||
+                (user.Players[i].position === "TE" && teCount === 2)) {
+                    rosterArray.push(user.Players[i]);
+                    flexCount++;
+                }
+            }
+        } 
+    }
+    for (let i = 0; i < user.Players.length; i++) {
+        if(user.Players[i].position === "DST" && dstCount < 1 ) {
+                rosterArray.push(user.Players[i]);
+                dstCount++;
+        }
+    }
+    for (let i = 0; i < user.Players.length; i++) {
+        if(user.Players[i].position === "K" && kCount < 1 ) {
+                rosterArray.push(user.Players[i]);
+                kCount++;
+        }
+    }
+    return rosterArray
 }
